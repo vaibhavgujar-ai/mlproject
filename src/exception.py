508e27af3c127990ec11b9a5536bb3b3f4360fea
vaibhav gujar <am22d401@smail.iitm.ac.in>
2026-02-04 16:21:06 +0530

    eda and model

--- src/exception.py
+++ src/exception.py
@@ -1,18 +1,25 @@
 import sys
-def error_message_detail(error, error_detail: sys):
-    _, _, exc_tb = error_detail.exc_info()
-    line_number = exc_tb.tb_lineno
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = f"Error occurred in script: {file_name} at line number: {line_number} with message: {str(error)}"
-    return error_message
+from src.logger import logging
+
+import sys
+import traceback
 
 class CustomException(Exception):
+
     def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail)
+        self.error_message = CustomException.get_detailed_error(
+            error_message, error_detail
+        )
+
+    @staticmethod
+    def get_detailed_error(error, error_detail: sys):
+        _, _, tb = error_detail.exc_info()
+        file_name = tb.tb_frame.f_code.co_filename
+        line_no = tb.tb_lineno
+
+        return f"Error occurred in script [{file_name}] at line [{line_no}] : {str(error)}"
 
     def __str__(self):
         return self.error_message
-    
-
 
